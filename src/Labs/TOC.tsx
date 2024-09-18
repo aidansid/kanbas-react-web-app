f745bb80188d9ce5958efbe2c2296dd447e569a4
aidansidloski <onefouriot@gmail.com>
2024-09-18 19:01:19 -0400

    A1 v4

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 export default function TOC() {
     return (
       <ul>
+        Aidan Sidloski
         <li><Link to="Labs">Labs</Link></li>
         <li><Link to="Lab1">Lab 1</Link></li>
         <li><Link to="Lab2">Lab 2</Link></li>
         <li><Link to="Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <div id="wd-github">
-          <li><a href="google.com">github</a></li>
-          <li><Link to="google.com">github</Link></li>
-        </div>
+        <li><a id="wd-github" href="https://github.com/aidansid/kanbas-react-web-app">github</a></li>
       </ul>
     );
   }
