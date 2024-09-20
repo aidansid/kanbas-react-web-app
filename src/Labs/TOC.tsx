846d09ded5b787dbae5224cd97f8988ab567e266
aidansidloski <onefouriot@gmail.com>
2024-09-20 11:35:27 -0400

    A1 v5

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,6 +4,7 @@ export default function TOC() {
     return (
       <ul>
         Aidan Sidloski
+        Section CS4550 02 F24
         <li><Link to="Labs">Labs</Link></li>
         <li><Link to="Lab1">Lab 1</Link></li>
         <li><Link to="Lab2">Lab 2</Link></li>
